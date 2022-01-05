9f7c655046c417705c4306ee65f40ca09e41e5e3
Kush Dhuvad <kushdhuvad9@gmail.com>
2022-01-05 20:53:41 +0530

    Subarray with 0 sum

---
+++ arrays/sum_zero.py
@@ -0,0 +1,15 @@
+#Subarray with 0 sum
+
+def subArrayExists(arr,n):
+    n_sum=0
+    s=set()
+    for i in range(n):
+        n_sum+=arr[i]
+        if n_sum==0 or n_sum in s:
+            return True
+        s.add(n_sum)
+    return False
+
+arr=[4,2,0,1,6]
+n=len(arr)
+print(subArrayExists(arr,n))
